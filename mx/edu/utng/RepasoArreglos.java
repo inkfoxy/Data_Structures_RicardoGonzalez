5753bd3bad6ded57a193354876649b5cc0a35468
inkfoxy <inkfoxy74@gmail.com>
2026-09-18 14:06:53 -0600

    editado Repaso Arreglos

--- mx/edu/utng/RepasoArreglos.java
+++ mx/edu/utng/RepasoArreglos.java
@@ -3,5 +3,14 @@ public class RepasoArreglos {
 	
 	public static void main(String[] args){
 		System.out.println("Manejando Arreglos");
+		
+		String[] nombres = new String[10];
+		int edades[] = {20,21,22,23};
+		System.out.println("tamaño " + nombres.length);
+		System.out.println("Edades " + edades.length);
+
+		//indices
+		nombres[0] = "juan Alberto";
+		//nombres[11] = "Juan Felipe";
 	}
 }
